217ea3974f0c24150f640495c7ce3251b1339665
Adnan Sabbir <adnan.sabbir@shohoz.com>
2021-03-28 23:45:50 +0600

    Added join component

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,47 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {Route, RouterModule, Routes} from '@angular/router';
+import {AuthComponent} from './pages/auth/auth.component';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {FlexModule} from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { JoinComponent } from './components/join/join.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'join',
+  },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+      {
+        path: 'join',
+        component: JoinComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [AuthComponent, LoginComponent, RegisterComponent, JoinComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule,
+    FlexModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ]
+})
+export class AuthModule {
+}
